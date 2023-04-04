c0774a2663e0cb7da627412052e07bf205e48b74
Булгаков Денис Игоревич <bulgakov.denis@russianpost.ru>
2023-04-05 00:19:17 +0300

    fix: fixed project build for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
