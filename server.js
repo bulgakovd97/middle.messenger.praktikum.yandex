b554682588cc8ae7de20e6928a225073d98ba0a6
Денис Булгаков <bulgakov_denis@Noutbuk-Denis.local>
2023-06-11 03:46:30 +0300

    fix: fixing routing tests error

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(`${__dirname}/dist`));
 
 app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'dist/index.html'));
   res.send('Not Found');
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 app.listen(PORT, () => {
